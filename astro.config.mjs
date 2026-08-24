645ab655520698fde384a53019b059365a733a2f
kyungmi-cho <jogaemi@gmail.com>
2026-08-24 19:12:44 +0900

    경로 설정 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 
-// 배포 시 실제 블로그 주소로 변경하세요.
-// 예) https://www.starteacher.co.kr/blog  또는  https://blog.starteacher.co.kr
 export default defineConfig({
-  site: 'https://www.starteacher.co.kr',
+  // 깃허브 페이지 기본 도메인
+  site: 'https://kyungmi-cho.github.io', 
+  // 레포지토리 이름 (반드시 맨 앞에 슬래시 / 를 붙여야 합니다)
+  base: '/starteacher-blog', 
   trailingSlash: 'ignore',
 })
